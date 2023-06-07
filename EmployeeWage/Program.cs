747d3922726d21094ba0de3994cc4f9026a785b5
unknown <bhotnikhil25@gmail.com>
2023-06-07 20:56:12 +0530

    [Nikhil]-Wel-come To Employee Wage Computation Problem

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
+            Console.WriteLine("Wel-come To Employee Wage Computation Problem");
+        }
+    }
+}
