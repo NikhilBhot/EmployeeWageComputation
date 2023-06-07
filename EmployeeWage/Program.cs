d7111843d1de683ebeeaf6f9fe45be2ce7588b22
unknown <bhotnikhil25@gmail.com>
2023-06-07 21:00:38 +0530

    [Nikhil]-Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System;
+using System.Diagnostics.Metrics;
 
 namespace EmployeeWage
 {
@@ -8,6 +9,21 @@ namespace EmployeeWage
         {
             //Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
             Console.WriteLine("Wel-come To Employee Wage Computation Problem");
+
+            //UC-01-Check Employee is Present or Absent - Use((RANDOM)) for Attendance Check
+            
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
