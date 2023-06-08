469ec15b885a840abeb75b6b40525e10a4f700d7
unknown <bhotnikhil25@gmail.com>
2023-06-08 10:25:41 +0530

    [Nikhil]-Calculating Employee Wage Using Switch

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Diagnostics.Metrics;
 
 namespace EmployeeWage
 {
     public class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int IS_ABSENT = 0;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             //Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
@@ -15,31 +20,31 @@ namespace EmployeeWage
             //UC-02-Calculate Daily Employee Wage -Assume Wage per Hour is 20 - Assume Full Day Hour is 8
 
             //UC-03 -Add Part time Employee & Wage - Assume Part time Hour is 4
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int IS_ABSENT = 0;
-            int EMP_RATE_PER_HOUR = 20;
-
+            
+            //variable
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee Is Full Time");
-                empHrs = 8;
 
-            }
-            else if (empCheck == IS_PART_TIME)
+           //UC-04- Calculating Employee Wage Using Switch
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Part Time");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee Is Absent");
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee Is Full Time");
+                       empHrs = 8;
+                    break;
+            case IS_PART_TIME:
+                    Console.WriteLine("Employee is Part Time");
+                       empHrs = 4;
+                    break;
+            default:
+                    Console.WriteLine("Employee Absent");
+                    empWage = 0;
+                    break;
+            
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage:" + empWage);
