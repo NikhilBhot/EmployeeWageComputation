7bab55cd4387eaedc6931778b98fa21e6abdfe1d
unknown <bhotnikhil25@gmail.com>
2023-06-08 10:09:39 +0530

    [Nikhil]-Add Part time Employee & Wage- Assume Part time Hour is 4

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -13,7 +13,11 @@ namespace EmployeeWage
             //UC-01-Check Employee is Present or Absent - Use((RANDOM)) for Attendance Check
 
             //UC-02-Calculate Daily Employee Wage -Assume Wage per Hour is 20 - Assume Full Day Hour is 8
+
+            //UC-03 -Add Part time Employee & Wage - Assume Part time Hour is 4
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int IS_ABSENT = 0;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
@@ -21,17 +25,21 @@ namespace EmployeeWage
 
             Random random = new Random();
 
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee Is Present");
+                Console.WriteLine("Employee Is Full Time");
                 empHrs = 8;
 
             }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Part Time");
+                empHrs = 4;
+            }
             else
             {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                Console.WriteLine("Employee Is Absent");
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage:" + empWage);
